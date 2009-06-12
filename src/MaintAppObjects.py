13480adc8ccbb1f87844fc3758a8f173b2ac5459
bd.gaiser@gmail.com <bd.gaiser@gmail.com@bf17635e-4420-11de-bfec-d9b18e096ff9>
2009-06-12 20:45:54 +0000

    Made final changes needed to fully support wrapper objects only holding strings.

--- src/MaintAppObjects.py
+++ src/MaintAppObjects.py
@@ -8,6 +8,11 @@ back and forth between the View and the Model.  As such, it has to have some
 knowledge of the data requirements on both sides in order to minimize the
 need for special code on each side for reformatting data to meet either the
 requirements of the UI or of the Data Store.
+
+2009-06-11 Implemented __eq__ for deep comparison of objects      Les Faby
+2009-06-12 Changed design of wrapper objects to only hold string information
+           at all times.  Allowing the View to work with the fields directly
+           greatly simplifies the data handling in the template code.  bdg
 '''
 
 """ Wing, notice that I changed the Id value from None to -1 to indicate that
@@ -20,17 +25,16 @@ requirements of the UI or of the Data Store.
 """
 from datetime import *
 from time import *
+from Utilities import dateToString, myLog
 
-def nz(value):
-	return ("" if value is None else value)
     
 ###########################################################################################
 
 class Customer(object):
     """ Light weight class for passing the customer data around. """
-    def __init__(self, id="-1", first_name=None, last_name=None,
-                 address1=None, address2=None, city=None, state=None, zip=None,
-                 phone1=None, phone2=None, email=None, comments=None):
+    def __init__(self, id="-1", first_name="", last_name="",
+                 address1="", address2="", city="", state="", zip="",
+                 phone1="", phone2="", email="", comments=""):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
@@ -53,46 +57,25 @@ class Customer(object):
     def getId(self):
         return self.id
     
-    def setFirstName(self, first_name):
-        self.first_name = first_name
-        
-    def getFirstName(self):
-        return nz(self.first_name)
-    
-    def setLastname(self, last_name):
-        self.last_name = last_name
-        
-    def getLastName(self):
-        return nz(self.last_name)
-    
-    def setComments(self, comments):
-        self.comments = comments
-        
-    def getComments(self):
-        return nz(self.comments)
-    
     def loadFromDictionary(self, dictionary):
         """ Load values from dictionary passed over from the view. """
-        #customer_id = dictionary['customer_id']
-        #if customer_id == '-1':
-        #    self.setId(None)
-        #else:
-        #    self.setId(customer_id)
-        
         self.setId(dictionary['customer_id'])   
-        self.setFirstName(dictionary['first_name'])
-        self.setLastname(dictionary['last_name'])
+        self.first_name = dictionary['first_name']
+        self.last_name = dictionary['last_name']
         self.address1 = dictionary['address1']
+        self.address2 = dictionary['address2']
         self.city = dictionary['city']
         self.state = dictionary['state']
         self.zip = dictionary['zip']
         self.phone1 = dictionary['phone1']
+        self.phone2 = dictionary['phone2']
+        self.email = dictionary['email']
 
         # The 'comments' field is not available when searching.
         # Another choice here would be to hide the comments field
         # in search mode so this if statement wouldn't be needed.
         if 'comments' in dictionary.keys():
-            self.setComments(dictionary['comments'])
+            self.comments = dictionary['comments']
             
         return None
     
@@ -110,7 +93,6 @@ class Customer(object):
                 "\temail = " + self.email + "\n" + \
                 "\tcomments = " + self.comments + "\n"            
     
-    
     def __eq__(self, other):
         '''
         Overload the == operator to do a field-by-field comparison of self
@@ -120,7 +102,7 @@ class Customer(object):
         2 attributes can be compared using "=="
         '''
         for a in self.__dict__:
-            if (nz(getattr(self,a)) != nz(getattr(other,a))):
+            if (getattr(self,a) != getattr(other,a)):
                 return False 
         return True
 
@@ -134,8 +116,8 @@ class Customer(object):
     
 class Vehicle(object):
     """ Light weight class for passing the vehicle data around. """
-    def __init__(self, id="-1", customer_id=None, make=None, model=None,
-                 year=None, license=None, vin=None, notes=None):
+    def __init__(self, id="-1", customer_id="", make="", model="",
+                 year="", license="", vin="", notes=""):
         self.id = id
         self.customer_id = customer_id
         self.make = make
@@ -164,12 +146,6 @@ class Vehicle(object):
             Data authentication is being done on client side with
             javascript.
         """
-        # Load fields similarly to method sketched out in Customer class
-        #vehicle_id = dictionary['vehicle_id']
-        #if vehicle_id == '-1':
-        #    self.setId(None)
-        #else:
-        #    self.setId(vehicle_id)
         self.setId(dictionary['vehicle_id'])
         self.setCustomerId(dictionary['customer_id'])
         self.make = dictionary['make']
@@ -201,7 +177,7 @@ class Vehicle(object):
         2 attributes can be compared using "=="
         '''
         for a in self.__dict__:
-            if (nz(getattr(self,a)) != nz(getattr(other,a))):
+            if (getattr(self,a) != getattr(other,a)):
                 return False 
         return True
 
@@ -222,10 +198,10 @@ class Workorder(object):
     DATE_FORMAT = "%b %d, %Y  %H:%M:%S"
     NO_MECHANIC = "NO_MECHANIC"
     
-    def __init__(self, id="-1", vehicle_id=None, mileage=None,
-                 status=OPEN, date_created=None, customer_request=None,
-                 mechanic=None, task_list=None, work_performed=None,
-                 notes=None, date_closed=None):
+    def __init__(self, id="-1", vehicle_id="", mileage="",
+                 status=OPEN, date_created="", customer_request="",
+                 mechanic="", task_list="", work_performed="",
+                 notes="", date_closed=""):
         self.id = id
         self.vehicle_id = vehicle_id
         self.mileage = mileage
@@ -254,60 +230,50 @@ class Workorder(object):
         return self.vehicle_id
     
     def setDateCreated(self):
-        self.date_created = \
-            datetime.fromtimestamp(time())
+        self.date_created = dateToString(datetime.fromtimestamp(time()))
         
     def getDateCreated(self):
         """ Do we want to format this as a string or return it as a date/time type? """
-        if self.date_created == None:
-            return None
-        else:
-            return self.date_created.strftime(Workorder.DATE_FORMAT)
+        return self.date_created
+        #if self.date_created == None:
+        #    return None
+        #else:
+        #    return self.date_created.strftime(Workorder.DATE_FORMAT)
     
     def setDateClosed(self):
-        self.date_closed = \
-            datetime.fromtimestamp(time())
+        self.date_closed = dateToString(datetime.fromtimestamp(time()))
         
     def getDateClosed(self):
         """ Do we want to format this as a string or return it as a date/time type? """
-        if self.date_closed == None:
-            return None
-        else:
-            return self.date_closed.strftime(Workorder.DATE_FORMAT)
+        return self.date_closed
+        #if self.date_closed == None:
+        #    return None
+        #else:
+        #    return self.date_closed.strftime(Workorder.DATE_FORMAT)
     
     def loadFromDictionary(self, dictionary):
         """ Load values from dictionary passed over from the view. 
             Data authentication is being done on client side with
             javascript.
         """
-        # Load fields similarly to method sketched out in Customer class
-        #workorder_id = dictionary['workorder_id']
-        #if workorder_id == '-1':
-        #    self.setId(None)
-        #else:
-        #    self.setId(workorder_id)
-
-        #vehicle_id = dictionary['vehicle_id']
-        #if vehicle_id == '-1':
-        #    self.setVehicleId(None)
-        #else:
-        #    self.setVehicleId(vehicle_id)
         self.setId(dictionary['workorder_id'])
         self.setVehicleId(dictionary['vehicle_id'])
         self.customer_request = dictionary['customer_request']
         self.mileage = dictionary['mileage']
-        strDate = dictionary['date_created']
-        if strDate == "":
-            self.date_created = None
-        else:
-            self.date_created = \
-                datetime.strptime(strDate, Workorder.DATE_FORMAT)
-        strDate = dictionary['date_closed']
-        if strDate == "":
-            self.date_closed = None
-        else:
-            self.date_closed = \
-                datetime.strptime(strDate, Workorder.DATE_FORMAT)
+        self.date_created = dictionary['date_created']
+        #strDate = dictionary['date_created']
+        #if strDate == "":
+        #    self.date_created = None
+        #else:
+        #    self.date_created = \
+        #        datetime.strptime(strDate, Workorder.DATE_FORMAT)
+        self.date_closed = dictionary['date_closed']
+        #strDate = dictionary['date_closed']
+        #if strDate == "":
+        #    self.date_closed = None
+        #else:
+        #    self.date_closed = \
+        #        datetime.strptime(strDate, Workorder.DATE_FORMAT)
         mechanic = dictionary['mechanic']
         if mechanic == Workorder.NO_MECHANIC:
             self.mechanic = None
@@ -320,23 +286,6 @@ class Workorder(object):
         
         return None
     
-    def checkRequiredFieldsForCurrentState(self):
-        """ Checks that the work order has all of the fields needed when
-            work order is to be saved to database for the given internal
-            status.  This should only report errors that are beyond the
-            ones reported by MaintAppModel.validateWorkorderInfo().
-        """
-        if self.status == Workorder.COMPLETED or self.status == Workorder.CLOSED:
-            # Check the final required fields for the work order and
-            # generate list.
-            retVal=[]
-        else:
-            # In the open state, the database validation is sufficient so this
-            # routine does not need to generate additional errors.
-            retVal = []
-        return retVal
-    
-     
     def __eq__(self, other):
         '''
         Overload the == operator to do a field-by-field comparison of self
@@ -346,7 +295,8 @@ class Workorder(object):
         2 attributes can be compared using "=="
         '''
         for a in self.__dict__:
-            if (nz(getattr(self,a)) != nz(getattr(other,a))):
+            if (getattr(self,a) != getattr(other,a)):
+                myLog.write(">>>> %s: %s != %s" % (a, getattr(self,a), getattr(other,a)))
                 return False 
         return True
 
