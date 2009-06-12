0f4cb9ad855468315c3b62a984dd822a6bd211c7
bd.gaiser@gmail.com <bd.gaiser@gmail.com@bf17635e-4420-11de-bfec-d9b18e096ff9>
2009-06-12 19:38:35 +0000

    Renamed from module util.  Also added dateToString, stringToDate, and shortDate functions to support cleanup of wrapper classes holding only string values.

---
+++ src/Utilities.py
@@ -0,0 +1,44 @@
+import sys
+from datetime import datetime
+
+class MyLog:   
+    def write(self,msg):
+        print >> sys.stderr, msg
+        
+myLog = MyLog()
+
+DATE_FORMAT = "%b %d, %Y  %H:%M:%S"
+SHORT_DATE_FORMAT = "%b %d, %Y"
+    
+def dateToString(date, format=DATE_FORMAT):
+    """ If none, return empty string, otherwise return string version of date. """
+    if date == None:
+        return ""
+    else:
+        return date.strftime(format)
+    
+def stringToDate(strDate):
+    """ If empty string, return None, otherwise return datetime object 
+        corresponding to string formatted date.  Should be inverse of
+        dateToString function. """
+    if strDate == "":
+        return None
+    else:
+        return datetime.strptime(strDate, DATE_FORMAT)
+
+def shortDate(strDate):
+    """ Take the standard string date representation and return a short date """
+    return dateToString(stringToDate(strDate), SHORT_DATE_FORMAT)
+
+
+################################################################################
+
+def main():
+    date = datetime.now()
+    print str(date)
+    print dateToString(date)
+    print shortDate(dateToString(date))
+    print str(stringToDate(dateToString(date)))
+    
+if __name__ == "__main__":
+    main()
